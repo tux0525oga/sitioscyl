10b374e62cff33e7e7a2f418635add942afef974
Oswaldo Garcia Segura <USRMEX30@sofagro.mx>
2026-08-31 19:52:19 -0600

    Add Somos Constructivos favicon

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{ asset('sc-favicon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('sc-favicon.png') }}">  
 
     @php
         $pageTitle = trim(
